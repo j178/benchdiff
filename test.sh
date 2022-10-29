5ea14645ca715611b7776bf983ea1f7e98cf6f11
j178 <10510431+j178@users.noreply.github.com>
2022-10-29 23:45:16 +0800

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-go test -bench . -run '^$' -count 5 > result.txt
-go run main.go -pat 'strings.Builder,bytes.Buffer' result.txt
+go test -bench . -benchmem -run '^$' -count 5 > result.txt
+go run main.go -name 'Builder-vs-Buffer' -pat 'strings.Builder,bytes.Buffer' result.txt
